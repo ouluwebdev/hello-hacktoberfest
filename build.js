c2463b26ea55850ac1a2f7db4172cac446756d2f
Daniel Reed <daniel.reed@avalanchestudios.se>
2018-10-02 14:00:12 -0700

    added prism with auto prefixing included

--- build.js
+++ build.js
@@ -6,12 +6,13 @@ fse.copySync("public", "build", {
   overwrite: true,
 });
 
+fse.copySync('src', 'build/src', {overwrite:true});
+
 const filenames = fs.readdirSync("src");
 
 const snippets = filenames.map((filename) => {
   const content = fs.readFileSync(path.join("src", filename), "utf-8");
-
-  return `<pre><code>${content}</pre></code>`;
+  return `<pre data-src="./src/${filename}"></pre>`;
 });
 
 const INDEX_PATH = path.join(__dirname, "build", "index.html");
@@ -20,7 +21,7 @@ const INDEX = fs.readFileSync(INDEX_PATH, "utf-8").split(INDEX_MARKER)[0];
 
 const snippetsHTML = snippets.join("\n");
 
-const newIndex = `${INDEX}${INDEX_MARKER}\n${snippetsHTML}</body></html>`;
+const newIndex = `${INDEX}${INDEX_MARKER}\n${snippetsHTML}<script src="./prism/prism.js"></script></body></html>`;
 
 fs.writeFileSync(INDEX_PATH, newIndex, "utf-8");
 console.log("Updated index.html with snippets:", snippetsHTML);
